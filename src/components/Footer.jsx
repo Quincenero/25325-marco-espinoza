bec04c331ede31d465745a2e23e33119dda58f8d
Marco <mespinozapoma@gmail.com>
2025-11-30 18:22:27 -0300

    correccion de errores en Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 function Footer() {
   return (
@@ -12,16 +14,16 @@ function Footer() {
           </div>
 
           <div className="col-md-4 mb-3">
-            <h5>Enlaces utiles</h5>
+            <h5 className="fw-bold">Enlaces utiles</h5>
             <ul className="list-unstyled">
-              <li><a href="/" className="text-light text-decoration-none">Inicio</a></li>
-              <li><a href="/productos" className="text-light text-decoration-none">Productos</a></li>
-              <li><a href="/contacto" className="text-light text-decoration-none">Contacto</a></li>
+              <li><Link to="/" className="text-light text-decoration-none">Inicio</Link></li>
+              <li><Link to="/productos" className="text-light text-decoration-none">Productos</Link></li>
+              <li><Link to="/contacto" className="text-light text-decoration-none">Contacto</Link></li>
             </ul>
           </div>
           
           <div className="col-md-4 mb-3">
-            <h5>Seguinos: </h5>
+            <h5 className="fw-bold">Seguinos: </h5>
             <a href="#" className="text-light me-3"><i className="bi bi-facebook"></i></a>
             <a href="#" className="text-light me-3"><i className="bi bi-instagram"></i></a>
             <a href="#" className="text-light me-3"><i className="bi bi-twitter"></i></a>
