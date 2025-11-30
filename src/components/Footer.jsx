e715c61b1bb31a315fa00b83ff8350d8b70a2b18
Marco <mespinozapoma@gmail.com>
2025-11-30 16:27:06 -0300

    correccion de Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,24 +12,26 @@ function Footer() {
           </div>
 
           <div className="col-md-4 mb-3">
-            <h5>Enlaces</h5>
+            <h5>Enlaces utiles</h5>
             <ul className="list-unstyled">
               <li><a href="/" className="text-light text-decoration-none">Inicio</a></li>
               <li><a href="/productos" className="text-light text-decoration-none">Productos</a></li>
-              <li><a href="#" className="text-light text-decoration-none">Sobre nosotros</a></li>
-              <li><a href="#" className="text-light text-decoration-none">Contacto</a></li>
+              <li><a href="/contacto" className="text-light text-decoration-none">Contacto</a></li>
             </ul>
           </div>
           
           <div className="col-md-4 mb-3">
-            <h5>Redes Sociales</h5>
+            <h5>Seguinos: </h5>
             <a href="#" className="text-light me-3"><i className="bi bi-facebook"></i></a>
             <a href="#" className="text-light me-3"><i className="bi bi-instagram"></i></a>
-            <a href="#" className="text-light"><i className="bi bi-twitter"></i></a>
+            <a href="#" className="text-light me-3"><i className="bi bi-twitter"></i></a>
+            <a href="#" className="text-light me-3"><i className="bi bi-tiktok"></i></a>
+            <a href="#" className="text-light me-3"><i className="bi bi-youtube"></i></a>
+            <a href="#" className="text-light me-3"><i className="bi bi-whatsapp"></i></a>
           </div>
         </div>
         <hr className="bg-light" />
-        <p className="mb-0">&copy; {new Date().getFullYear()} EcoMarket. Todos los derechos reservados.</p>
+        <p className="mb-0 text-info">&copy; {new Date().getFullYear()} EcoMarket. Todos los derechos reservados.</p>
       </div>
     </footer>
   )
